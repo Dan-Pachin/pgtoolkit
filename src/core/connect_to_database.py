5276723922383b10ad6c39e96365c7b36de7c604
Dan-Pachin <danil@dattis.ru>
2025-08-06 12:02:45 +0800

    Update: add host param to psycopg2 connection

--- src/core/connect_to_database.py
+++ src/core/connect_to_database.py
@@ -5,13 +5,13 @@ import psycopg2.extensions
 
 def connect_to_database(database_input="postgres", user_input="postgres"):
     try: 
-        connection = psycopg2.connect(dbname=database_input, user=user_input) 
+        connection = psycopg2.connect(dbname=database_input, user=user_input, host="127.0.0.1") 
         connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
         print(f"\u2714 Database conntection successfully established {connection}")
         return connection
     except psycopg2.OperationalError as e:
         password_input = input("Please enter password for user 'postgres': ")
-        connection = psycopg2.connect(dbname=database_input, user=user_input, password = password_input) 
+        connection = psycopg2.connect(dbname=database_input, user=user_input, password = password_input, host="127.0.0.1") 
         connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
         print(f"\u2714 Database conntection successfully established via password {connection}")
         return connection
